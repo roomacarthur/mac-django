4d73a8e10edd67b8004c13d5564b775af7e69354
Roo Macarthur <roomacarthur@gmail.com>
2024-12-11 14:02:38 +0000

    Adds: social path logic, and link to navbar

--- config/context_processors.py
+++ config/context_processors.py
@@ -4,4 +4,5 @@ def active_nav_context(request):
         'is_forum_active': request.path.startswith('/forum'),
         'is_chat_active': request.path.startswith('/chat'),
         'is_booking_active': request.path.startswith('/booking'),
+        'is_social_active': request.path.startswith('/social'),
     }
